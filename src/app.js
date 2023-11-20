72640f921253ffecb715df330a5e9be149ec2292
Jason López <j4ckson95@gmail.com>
2023-11-20 07:12:52 -0500

    Adelanto

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from "express"
+import handlebars from "express-handlebars"
+import mongoose from "mongoose"
+
+const app = express()
+
+app.listen(8080, () => console.log("Running server ...<<(--_--)>>"))
